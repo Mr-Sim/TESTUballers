114e7a35637b4aa84af67e7c1f0a8a14359e7fa8
Simon Catanese <simoncatanese.pro@gmail.com>
2021-04-25 23:57:30 +0200

    application de certains changements

--- connect-db.php
+++ connect-db.php
@@ -5,9 +5,11 @@
  * Il sera utilisé par les fonctions executant des requêtes SQL. 
  */
 try{
+    //Modifiez les valeurs de ces trois variables en fonctions de vos paramètres de connextion à la base de donnée.
     $host = 'mysql:host=localhost;dbname=utilisateurs_test;charset=utf8';
     $login = 'root';
     $password = '';
+
     $pdo = new PDO( $host, $login, $password ) ;
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
